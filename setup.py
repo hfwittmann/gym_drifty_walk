a878638818db9eca3f3c0b5456285cff39e38371
hfwittmann <hfwittmann@gmail.com>
2017-12-16 12:05:55 +0100

    Fourth commit, naming hyphen to underscore

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='gym-drifty_walk',
+setup(name='gym_drifty_walk',
       version='0.0.1',
       install_requires=['gym'],
       packages=find_packages()
